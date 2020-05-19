fa41a8e4c5ad36217855d193500f521519a61d9e
sukh <sukhveer.sangha@tatacommunications.com>
2020-05-19 12:25:48 +0100

    removed old interceptor with componentDidUnmount and tested with state in App.js

--- src/hoc/ErrorHandler/ErrorHandler.js
+++ src/hoc/ErrorHandler/ErrorHandler.js
@@ -10,17 +10,22 @@ const errorHandler = (WrappedComponent, axios) => {
         }
 
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({
                     error: null // clears the error
                 })
                 return req
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({
                     error: error
                 })
             })
+        }
+
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInterceptor)
+            axios.interceptors.response.eject(this.resInterceptor)
         }
 
         errorConfirmedHandler = () => {
