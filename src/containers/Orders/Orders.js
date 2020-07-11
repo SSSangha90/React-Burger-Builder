8f30886d44cf377787fb53b6391b0ba831f2d055
sukh <sukhveer.sangha@tatacommunications.com>
2020-07-11 11:13:06 +0100

    Fetched orders from Google Firebase DB using Axios

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react'
 
+import axios from '../../axios-orders'
 import Order from '../../components/Order/Order'
+import ErrorHandler from '../../hoc/ErrorHandler/ErrorHandler'
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount(){
+        // To fetch the orders
+        axios.get('orders.json')
+        .then(res => {
+            const fetchedOrders = []
+            // convert object response into an array
+            for(let key in res.data){
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                })
+            }
+            this.setState({
+                loading: false,
+                orders: fetchedOrders
+            })
+        })
+        .catch(err => {
+            this.setState({
+                loading: false
+            })
+        })
+    }
     render(){
         return (
             <div>
@@ -13,4 +42,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders
+export default ErrorHandler(Orders, axios)
