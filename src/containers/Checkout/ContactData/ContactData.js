9c0a36830e85eb8150a29019ceea5fd6e8a8feef
sukh <sukhveer.sangha@tatacommunications.com>
2020-07-13 16:47:17 +0100

    Created customer Input component for dynamic form, and styled

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import axios from '../../../axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
@@ -45,10 +46,10 @@ class ContactData extends Component {
     render (){
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                <input className={classes.Input} type="text" name="email" placeholder="Your email" />
-                <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                <input className={classes.Input} type="text" name="postcode" placeholder="Postcode" />
+                <Input inputtype="input" type="text" name="name" placeholder="Your name" />
+                <Input inputtype="input" type="text" name="email" placeholder="Your email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postcode" placeholder="Postcode" />
                 <Button 
                     clicked={this.orderHandler}
                     btnType="Success"
