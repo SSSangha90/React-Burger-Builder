87c37465124a8701446850d70914e49f0f90179f
sukh <sukhveer.sangha@tatacommunications.com>
2020-07-25 13:38:38 +0100

    handled form submission to firebase with orderForm handler

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,10 +67,15 @@ class ContactData extends Component {
         this.setState({
             loading: true
         })
+        const formData = {}
+        for (let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+            // key value pairs
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            
+            orderData: formData
         }
         axios.post('/orders.json', order)
         .then(res => {
@@ -103,7 +108,7 @@ class ContactData extends Component {
             })
         }
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map((formElement) => (
                     <Input 
                         key={formElement.id}
@@ -113,11 +118,7 @@ class ContactData extends Component {
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}
                     />
                 ))}
-                <Button 
-                    clicked={this.orderHandler}
-                    btnType="Success"
-                    >ORDER
-                </Button>
+                <Button btnType="Success">ORDER</Button>
             </form>
         )
         if(this.state.loading){
