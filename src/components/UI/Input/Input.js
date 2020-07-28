d21317851f64deb99156281055cf3637ccda3f49
sukh <sukhveer.sangha@tatacommunications.com>
2020-07-28 18:04:32 +0100

    Added form validation feedback using CSS

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,25 +4,30 @@ import classes from './Input.css'
 const input = (props) => {
 
     let inputElement = null
+    const inputClasses = [classes.inputElement]
+
+    if(props.invalid && props.shouldValidate){
+        inputClasses.push(classes.Invalid)
+    }
 
     switch(props.elementType){
         case('input'):
             inputElement = <input 
-                className={classes.InputElement} 
+                className={inputClasses.join(' ')} 
                 {...props.elementConfig} 
                 value={props.value}
                 onChange={props.changed}/>
             break
         case('textarea'):
             inputElement = <textarea 
-                className={classes.InputElement} 
+                className={inputClasses.join(' ')} 
                 {...props.elementConfig} 
                 value={props.value}
                 onChange={props.changed}/>
             break
         case('select'):
             inputElement = <select 
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 value={props.value}
                 onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
@@ -31,7 +36,10 @@ const input = (props) => {
                 </select>
         break
         default:
-            inputElement = <input className={classes.InputElement}  value={props.value} onChange={props.changed}/>
+            inputElement = <input 
+            className={inputClasses.join(' ')}  
+            value={props.value} 
+            onChange={props.changed}/>
     }
 
     return (
