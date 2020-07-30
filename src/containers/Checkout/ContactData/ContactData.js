ccbb4ca2cc1040e3f5e494a3353b0fab02693ec3
sukh <sukhveer.sangha@tatacommunications.com>
2020-07-30 17:04:29 +0100

    improved overall form validity, including order button disabled on incomplete form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,8 +84,10 @@ class ContactData extends Component {
                     ]
                 },
                 value: '',
+                valid: true,
             },
         },
+        formIsValid: false,
         loading: false,
     }
 
@@ -137,9 +139,14 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
         updatedFormElement.touched = true
         updatedOrderForm[inputIdentifier] = updatedFormElement
-        console.log(updatedFormElement)
+
+        let formIsValid = true
+        for (let inputIdentifier in updatedOrderForm){
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+        }
         this.setState({
-            orderForm: updatedOrderForm
+            orderForm: updatedOrderForm, 
+            formIsValid: formIsValid
         })
     }
 
@@ -165,7 +172,7 @@ class ContactData extends Component {
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}
                     />
                 ))}
-                <Button btnType="Success">ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         )
         if(this.state.loading){
